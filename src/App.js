c76ec9ddbd923d613d742a3d55a34becc6694714
Alexxs <irreligious86@gmail.com>
2021-07-20 19:53:47 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+
+    const arr = [1, 2, 3, 4, 5];
+
+
+    return <ul>
+        {arr.map((i, index) => (
+            <li key={index}>
+                {i}
+            </li>
+        ))}
+    </ul>;
 }
 
 export default App;
