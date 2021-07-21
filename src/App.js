d2798604d17904a526a30dd09e71c41dbecd822e
Alexxs <irreligious86@gmail.com>
2021-07-21 19:20:40 +0200

    Fix id

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import React from 'react';
 function App() {
 
     const users = [
-        {name: 'user1', surn: 'surn1', age: 30},
-        {name: 'user2', surn: 'surn2', age: 31},
-        {name: 'user3', surn: 'surn3', age: 32},
+        {id:1, name: 'user1', surn: 'surn1', age: 30},
+        {id:2, name: 'user2', surn: 'surn2', age: 31},
+        {id:3, name: 'user3', surn: 'surn3', age: 32},
     ];
 
-    const list = users.map( (item, index) => {
-        return <li key={index}>
+    const list = users.map( (item) => {
+        return <li key={item.id}>
+            <span>{item.id + ' '}</span>
             <span>{item.name + ' '}</span>
             <span>{item.surn + ' '}</span>
             <span>{item.age + ' '}</span>
