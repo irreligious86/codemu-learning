d1bad8276d1a1733773c81a8a32ff956e4ebf1c3
Alexxs <irreligious86@gmail.com>
2021-07-21 19:17:15 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React from 'react';
 
 function App() {
 
-    return (
+    const users = [
+        {name: 'user1', surn: 'surn1', age: 30},
+        {name: 'user2', surn: 'surn2', age: 31},
+        {name: 'user3', surn: 'surn3', age: 32},
+    ];
 
-        <p>hello!</p>
+    const list = users.map( (item, index) => {
+        return <li key={index}>
+            <span>{item.name + ' '}</span>
+            <span>{item.surn + ' '}</span>
+            <span>{item.age + ' '}</span>
+        </li>
+    })
 
-    )
-}
+    return <ul>
+        {list}
+    </ul>
+
+
+    }
 
 export default App;
