c1fd5b6f41968f94216d6219774e8c7890bf5a3d
Alexxs <irreligious86@gmail.com>
2021-07-21 19:48:45 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { nanoid } from 'nanoid';
 
 function App() {
 
     return (
-
-        <p>hello!</p>
-
+<>
+    <p>hello! I am <i><b>Nanoid</b></i>, I can generate random ID</p>
+    <p>{nanoid()}</p>
+    <p>{nanoid(8)}</p>
+    <p>{nanoid(32)}</p>
+    <p>{nanoid(128)}</p>
+    <p>{nanoid(1024)}</p>
+</>
     )
 }
 
