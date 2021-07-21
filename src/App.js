df1e33bd38f8869aebcfeb7c76037f314ccffbcf
Alexxs <irreligious86@gmail.com>
2021-07-21 17:54:22 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 function App() {
+    function show1() {
+        alert(1);
+    }
 
-    return (
+    function show2() {
+        alert(2);
+    }
 
-        <p>hello!</p>
-
-    )
+    return <div>
+        <button onClick={show1}>act1</button>
+        <button onClick={show2}>act2</button>
+    </div>;
 }
 
 export default App;
