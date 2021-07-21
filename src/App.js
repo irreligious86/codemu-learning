75e4bd376a29adc650c4224c43c02785f607a5a0
Alexxs <irreligious86@gmail.com>
2021-07-21 21:17:36 +0200

    Complete changes 17.4

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     const [name, setName] = useState('John');
     const [surname, setSurname] = useState('Connor');
     const [age, setAge] = useState('16');
+    const [allowed, setAllowed] = useState(true);
 
 
     return (
@@ -12,7 +13,7 @@ function App() {
             <p>{name} <button onClick={()=>{setName('George')}}>Set name</button> </p>
             <p>{surname} <button onClick={()=>{setSurname('Black')}}>Set surname</button> </p>
             <p>{age} <button onClick={()=>{setAge(48)}}>Set age</button> </p>
-
+            <p>{ allowed ? 'Allowed' : 'Forbidden' } <button onClick={()=>{setAllowed(!allowed)}}>Allow</button> </p>
         </>
     )
 }
