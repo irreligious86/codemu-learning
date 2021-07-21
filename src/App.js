f5263d703d33f5c56ff9e36680153cffb3db5600
Alexxs <irreligious86@gmail.com>
2021-07-21 19:55:51 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
     return (
+<>
+    <p>hello! I am <i><b>React UUID</b></i>, I can generate random ID</p>
+    <p>{uuidv4()}</p>
+    <p>{uuidv4(32)}</p>
+    <p>{uuidv4(128)}</p>
+    <p>{uuidv4(1024)}</p>
 
-        <p>hello!</p>
-
+</>
     )
 }
 
