9a0538dacc89db7e95211c84666b2aa095619a5d
Alexxs <irreligious86@gmail.com>
2021-07-21 20:57:00 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React, { useState } from 'react';
 
 function App() {
 
+    const [name, setName] = useState('John');
+    const [surname, setSurname] = useState('Connor');
+    const [age, setAge] = useState('16');
+
+
     return (
+        <>
+            <p>{name}</p>
+            <p>{surname}</p>
+            <p>{age}</p>
 
-        <p>hello!</p>
-
+        </>
     )
 }
 
