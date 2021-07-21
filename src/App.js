e7f8c304c94b8a02fe003c27154d6decaa21339c
Alexxs <irreligious86@gmail.com>
2021-07-21 23:20:48 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+    const [value1, setValue1] = useState('text1');
+    const [value2, setValue2] = useState('text2');
 
-    return (
+    return <div>
+        <input value={value1} onChange={event => {setValue1(event.target.value)}}/>
+        <input value={value2} onChange={event => {setValue2(event.target.value)}}/>
 
-        <p>hello!</p>
-
-    )
+        <p>text1: {value1}</p>
+        <p>text2: {value2}</p>
+    </div>;
 }
 
 export default App;
