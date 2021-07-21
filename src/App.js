7ff47b5d13473e417082aef051450e209ce574fd
Alexxs <irreligious86@gmail.com>
2021-07-21 19:26:48 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 
+const users = [
+    {id: 1, name: 'user1', surn: 'surn1', age: 30},
+    {id: 2, name: 'user2', surn: 'surn2', age: 31},
+    {id: 3, name: 'user3', surn: 'surn3', age: 32},
+];
+
 function App() {
+    const rows = users.map(function(item) {
+        return <tr key={item.id}>
+            <td>{item.name}</td>
+            <td>{item.surn}</td>
+            <td>{item.age}</td>
+        </tr>;
+    });
 
-    return (
-
-        <p>hello!</p>
-
-    )
+    return <table>
+        <thead>
+        <tr>
+            <td>name</td>
+            <td>surname</td>
+            <td>age</td>
+        </tr>
+        </thead>
+        <tbody>
+        {rows}
+        </tbody>
+    </table>;
 }
 
 export default App;
