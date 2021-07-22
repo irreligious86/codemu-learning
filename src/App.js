0f02c3eae90094938d1cbd6693d61cf503b44659
Alexxs <irreligious86@gmail.com>
2021-07-22 16:24:33 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+    const [value, setValue] = useState('');
 
-    return (
-
-        <p>hello!</p>
-
-    )
+    return <div>
+        <select value={value} onChange={event => setValue(event.target.value)}>
+            <option value="1">0-12</option>
+            <option value="2">13-17</option>
+            <option value="3">18-25</option>
+            <option value="4">26+</option>
+        </select>
+        <p>
+            {value === '1' && 'Your age group is: 0-12'}
+            {value === '2' && 'Your age group is: 13-17'}
+            {value === '3' && 'Your age group is: 18-25'}
+            {value === '4' && 'Your age group is: 26+'}
+        </p>
+    </div>;
 }
 
 export default App;
