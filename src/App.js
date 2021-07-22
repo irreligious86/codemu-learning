d6010786f33c27f4cdeae46d1ee57b11705d799c
Alexxs <irreligious86@gmail.com>
2021-07-22 14:52:38 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
 
+    const [htmlChecked, setHtmlChecked] = useState(false);
+    const [cssChecked, setCssChecked] = useState(false);
+    const [jsChecked, setJsChecked] = useState(false);
+
     return (
-
-        <p>hello!</p>
-
+        <>
+            <input type="checkbox" checked={htmlChecked} onChange={() => setHtmlChecked(!htmlChecked)}/> <b>HTML</b> <br/>
+            <input type="checkbox" checked={cssChecked} onChange={() => setCssChecked(!cssChecked)}/> <b>CSS</b> <br/>
+            <input type="checkbox" checked={jsChecked} onChange={() => setJsChecked(!jsChecked)}/> <b>Javascript</b> <br/>
+            <p>
+                <b>HTML </b> { htmlChecked ? '+' : '-' }
+            </p>
+            <p>
+                <b>CSS </b> { cssChecked ? '+' : '-' }
+            </p>
+            <p>
+                <b>Javascript </b> { jsChecked ? '+' : '-' }
+            </p>
+        </>
     )
 }
 
