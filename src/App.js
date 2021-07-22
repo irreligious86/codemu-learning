6eb40ddeeff5fdddd90de83c53ac0d1c64ddb101
Alexxs <irreligious86@gmail.com>
2021-07-22 14:17:46 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
 
+    const [value, setValue] = useState('');
+    const [result, setResult] = useState('');
+
+    function changeHandler (event) {
+        setValue(event.target.value);
+    };
+
+    function sum (num) {
+        return (num.split('')).reduce( (accu, current) => +accu + +current )
+    };
+
+
     return (
-
-        <p>hello!</p>
-
+        <>
+            <input
+                onChange={changeHandler}
+                onBlur={() => {
+                    value && setResult(sum(value));
+                }}
+            />
+            <p><i><b>Result: </b></i>{result}</p>
+        </>
     )
-}
+};
 
 export default App;
