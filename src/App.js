6e09a062262a48d8422ea5b1c7ba8dd61e33fdf3
Alexxs <irreligious86@gmail.com>
2021-07-23 01:24:53 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+    const [notes, setNotes] = useState( ['a', 'b', 'c', 'd', 'e']);
+    const [value, setValue] = useState('');
+
+    const result = notes.map((note, index) => {
+        return <li key={index}>{note}</li>;
+    });
+
+    function addItem() {
+        setNotes([...notes, value]);
+    }
+
+    function changeInput(event) {
+        setValue(event.target.value);
+    }
 
     return (
-
-        <p>hello!</p>
-
+        <>
+            <ul>{result}</ul> <br/>
+            <input value={value} onChange={changeInput} onBlur={addItem}/>
+        </>
     )
 }
 
