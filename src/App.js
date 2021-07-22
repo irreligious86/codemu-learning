ee1774f1339a9ea2b893475d1c64842ce69b37f7
Alexxs <irreligious86@gmail.com>
2021-07-23 00:42:02 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import './App.css';
 
 function App() {
 
+    const [notes, setNotes] = useState([1, 2, 3, 4, 5]);
+
+    function clickHandler () {
+        return setNotes( () => [...notes, (notes.length+1).toString()])
+    }
+
+    const resultArr = notes.map((item, index) => {
+        return <b key={index}> {item} </b>;
+    });
+
     return (
-
-        <p>hello!</p>
-
+        <>
+        <button onClick={clickHandler}>Add item</button> <br/>
+        <p>{resultArr}</p>
+        </>
     )
 }
 
