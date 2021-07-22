ec98938c2966def79f7e4cbe888982ca8cafb222
Alexxs <irreligious86@gmail.com>
2021-07-22 17:43:07 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
 
+    const [value, setValue] = useState(1);
+    const langs = ['HTML', 'CSS', 'JS'];
+
+    function changeHandler(event) {
+        setValue(event.target.value);
+    }
+
+    const list = langs.map((item, index) => {
+        return (
+            <>
+            <input
+                type="radio"
+                name="radio"
+                value={langs[index]}
+                key={index}
+                id={item.value}
+                checked={value === item.toString() ? true : false}
+                onChange={changeHandler}
+            />
+                <label htmlFor={item.value}>{langs[index]}</label> <br/>
+            </>
+        )
+    });
+
     return (
-
-        <p>hello!</p>
-
+        <>
+            {list}
+            <p><i><b>Your choice: </b></i> {value} {value === 'JS' &&  <i>Super!!!!</i>}</p>
+        </>
     )
 }
 
