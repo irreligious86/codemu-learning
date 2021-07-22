ff2c7d3cede08b8fbb1cc9f21ec6f2c4944c6e40
Alexxs <irreligious86@gmail.com>
2021-07-22 12:46:07 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
 
+    const [value1, setValue1] = useState(0);
+    const [value2, setValue2] = useState(0);
+    const [result, setResult] = useState(0);
+
+    function handleChange1(event) {
+        setValue1(event.target.value);
+        console.log(new Date(value1).getTime())
+    };
+
+    function handleChange2(event) {
+        setValue2(event.target.value);
+        console.log(new Date(value2).getTime())
+    };
+
+    const timestamp1 = new Date(value1).getTime();
+    const timestamp2 = new Date(value2).getTime();
+
     return (
-
-        <p>hello!</p>
-
+        <>
+            <input value={value1} onChange={handleChange1}/> <br/>
+            <input value={value2} onChange={handleChange2}/> <br/>
+            <button onClick={() => { setResult((timestamp1 - timestamp2)/(1000*3600*24))
+            }
+            }>Get difference
+            </button>
+            <br/>
+            <p><i><b>Result: </b></i>{result}</p>
+        </>
     )
 }
 
