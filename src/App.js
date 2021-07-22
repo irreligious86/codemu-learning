86e305b6c679ac40b8ec43d4a5aa50f2c03bf033
Alexxs <irreligious86@gmail.com>
2021-07-22 14:38:30 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
 
+    const [checked, setChecked] = useState(true);
+
     return (
-
-        <p>hello!</p>
-
+        <>
+            <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)} /> <br/>
+            <button onClick={ () => setChecked(!checked) }>Click</button> <br/>
+            <p>{checked ? 'Hello, User!' : 'Good bye!'}</p>
+        </>
     )
 }
 
