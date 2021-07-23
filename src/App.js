221a960dd02dff6e10af16a3ab81b67316cf20ed
Alexxs <irreligious86@gmail.com>
2021-07-23 20:06:27 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -3,11 +3,34 @@ import './App.css';
 
 function App() {
 
-    return (
+    function average(arr) {
+        let sum = 0;
 
-        <p>hello!</p>
+        for (const elem of arr) {
+            sum += +elem;
+        }
 
-    )
+        return sum/arr.length;
+    }
+
+    const [notes, setNotes] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+    function changeHandler(index, event) {
+        setNotes([...notes.slice(0, index), event.target.value, ...notes.slice(index + 1)]);
+    }
+
+    const result = notes.map((note, index) => {
+        return <input
+            key={index}
+            value={note}
+            onChange={event => changeHandler(index, event)}
+        />;
+    });
+
+    return <div>
+        {result}
+        {average(notes)}
+    </div>;
 }
 
 export default App;
