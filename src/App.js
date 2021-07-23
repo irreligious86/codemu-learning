84efdd8d9f28b63b83a7ede9885023978f0c85d7
Alexxs <irreligious86@gmail.com>
2021-07-23 19:46:13 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+    const [notes, setNotes] = useState([' a ', ' b ', ' c ', ' d ', ' e ']);
 
-    return (
+    const result = notes.map((note, index) => {
+        return <li key={index}>
+            {note}
+            <button onClick={() => remItem(index)}>Del</button>
+        </li>;
+    });
 
-        <p>hello!</p>
+    function remItem(index) {
+        setNotes([...notes.slice(0, index), ...notes.slice(index + 1)]);
+    }
 
-    )
+    return <ul>
+        {result}
+    </ul>; 
 }
 
 export default App;
