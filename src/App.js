d27370ec2f3ea636f3eec6bfaf16d54fcbf38316
Alexxs <irreligious86@gmail.com>
2021-07-24 14:18:08 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,32 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+    const [notes, setNotes] = useState([' a ', ' b ', ' c ', ' d ', ' e ']);
+    const [editNum, setEditNum] = useState(null);
 
-    return (
+    const result = notes.map((note, index) => {
+        return <li key={index}>
+            {note}
+            <button onClick={() => setEditNum(index)}>Edit</button>
+        </li>;
+    });
 
-        <p>hello!</p>
+    function changeItem(event) {
+        setNotes([...notes.slice(0, editNum), event.target.value, ...notes.slice(editNum + 1)]);
+    }
 
-    )
+    function clearInput () {
+        setEditNum('');
+    }
+
+    return <ul>
+        {result}
+        <input
+            value={editNum ? notes[editNum] : ''}
+            onChange={changeItem}
+            onBlur={clearInput}
+        />
+    </ul>;
 }
 
 export default App;
