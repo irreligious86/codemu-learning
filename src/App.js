d5bdb94f22780a01b25e148d1584d8bbdcb1e5a4
Alexxs <irreligious86@gmail.com>
2021-07-24 22:43:09 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -3,11 +3,31 @@ import './App.css';
 
 function App() {
 
-    return (
+    const [obj, setObj] = useState({
+        prop1: 'value1',
+        prop2: 'value2',
+        prop3: 'value3',
+    });
 
-        <p>hello!</p>
+    function clickHandler1 (event) {
+        setObj({...obj, ...{prop1: '!'}})
+    }
 
-    )
+    function clickHandler2 (event) {
+        setObj({...obj, ...{prop2: '%'}})
+    }
+
+    function clickHandler3 (event) {
+        setObj({...obj, ...{prop3: '@'}})
+    }
+
+
+    return <div>
+        <span>{obj.prop1}</span> <button onClick={clickHandler1}>Edit</button> <br/>
+        <span>{obj.prop2}</span> <button onClick={clickHandler2}>Edit</button> <br/>
+        <span>{obj.prop3}</span> <button onClick={clickHandler3}>Edit</button> <br/>
+    </div>;
+
 }
 
 export default App;
