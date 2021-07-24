bafd8cb7516d2bbae48835d5811ce60d09cb1631
Alexxs <irreligious86@gmail.com>
2021-07-24 23:40:29 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { useState } from 'react';
 import './App.css';
 
+const initDate = {
+    year:  2025,
+    month: 12,
+    day:   31,
+}
+
+const daysOfWeek = {
+    0: 'Sun',
+    1: 'Mon',
+    2: 'Tue',
+    3: 'Wed',
+    4: 'Thu',
+    5: 'Fri',
+    6: 'Sat'
+}
+
 function App() {
 
+    const resultDate = `${(initDate.year).toString()}-${(initDate.month).toString()}-${(initDate.day).toString()}`;
+
+    const resultDay = daysOfWeek[new Date(resultDate).getDay()];
+
     return (
-
-        <p>hello!</p>
-
+       <p>
+           {resultDate}
+           <br/>
+           {resultDay}
+       </p>
     )
 }
 
