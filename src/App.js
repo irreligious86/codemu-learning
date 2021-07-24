6e413680c40f92378f1a89e5ed9ba6e6dae2e136
Alexxs <irreligious86@gmail.com>
2021-07-25 00:11:00 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -19,16 +19,37 @@ const daysOfWeek = {
 
 function App() {
 
-    const resultDate = `${(initDate.year).toString()}-${(initDate.month).toString()}-${(initDate.day).toString()}`;
+    const [value1, setValue1] = useState(initDate.year);
+    const [value2, setValue2] = useState(initDate.month);
+    const [value3, setValue3] = useState(initDate.day);
+    const [resultDate, setResultDate] = useState(`${(initDate.year).toString()}-${(initDate.month).toString()}-${(initDate.day).toString()}`)
+
+    function changeHandler1 (event) {
+        setValue1(event.target.value);
+    }
+    function changeHandler2 (event) {
+        setValue2(event.target.value);
+    }
+    function changeHandler3 (event) {
+        setValue3(event.target.value);
+    }
+    function blurHandler () {
+        setResultDate(`${(value1).toString()}-${value2.toString()}-${value3.toString()}`)
+    }
 
     const resultDay = daysOfWeek[new Date(resultDate).getDay()];
 
     return (
-       <p>
-           {resultDate}
-           <br/>
-           {resultDay}
-       </p>
+        <>
+            <p>
+                <input value={value1} onChange={changeHandler1} onBlur={blurHandler}/> <i><b>Year</b></i> <br/>
+                <input value={value2} onChange={changeHandler2} onBlur={blurHandler}/> <i><b>Month</b></i> <br/>
+                <input value={value3} onChange={changeHandler3} onBlur={blurHandler}/> <i><b>Day</b></i> <br/>
+                {resultDate}
+                <br/>
+                {resultDay}
+            </p>
+        </>
     )
 }
 
